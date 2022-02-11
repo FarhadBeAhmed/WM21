ee469ee4921c3b43f639baab4823d2c5b7e19fe0
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-02-12 01:48:53 +0600

    ✅ Created Home Fragment

---
+++ app/src/main/java/co/wm21/https/HomeFragment.java
@@ -0,0 +1,59 @@
+package co.wm21.https;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.wm21.https.databinding.FragmentMainHomeBinding;
+import co.wm21.https.placeholder.PlaceholderContent;
+
+public class HomeFragment extends Fragment {
+
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private int mColumnCount = 1;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public HomeFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentMainHomeBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main_home, container, false);
+        View view = binding.getRoot();
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(new ProductRecyclerViewAdapter(PlaceholderContent.ITEMS));
+        }
+        return view;
+    }
+}
